bc69fbbe2b8585c9215fa4b536b3fd377184e6b8
Silvio Limeira <sicemal@gmail.com>
2023-02-28 10:50:14 -0400

    DELETE JDBC command with mariadb

--- src/main/java/CRUDTest.java
+++ src/main/java/CRUDTest.java
@@ -20,7 +20,14 @@ public class CRUDTest {
 			
 			// INSERT A NEW ROW INSERT COMMAND
 			//int n = statement.executeUpdate("INSERT INTO employee VALUES(103, 'Employee C', 7000)");
-			int n = statement.executeUpdate("INSERT INTO employee(emp_id, emp_name) VALUES(104, 'Employee D')");
+			//int n = statement.executeUpdate("INSERT INTO employee(emp_id, emp_name) VALUES(104, 'Employee D')");
+			
+			// UPDATE ROW COMMAND
+			//int n = statement.executeUpdate("UPDATE employee SET emp_salary = 11000 WHERE emp_id = 104");
+			//int n = statement.executeUpdate("UPDATE employee SET emp_salary = 5000 WHERE emp_salary > 1000");
+			
+			// DELETE RECORD COMMAND
+			int n = statement.executeUpdate("DELETE FROM empoyee WHERE emp_id = 104");
 			
 			System.out.println("Number of rows affected: " + n);
 			
