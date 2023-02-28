7c471c4d5327dddba74645d7f1eee007bfb206ff
Silvio Limeira <sicemal@gmail.com>
2023-02-28 11:16:08 -0400

    Insert using PreparedStatement repord

--- src/main/java/CRUDTest.java
+++ src/main/java/CRUDTest.java
@@ -1,7 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 
 public class CRUDTest {
 
@@ -14,20 +13,12 @@ public class CRUDTest {
 					"jdbc:mariadb://localhost:3306/mydb?createDatabaseIfNotExist=true&useSsl=false&useUnicode=true&characterEncoding=UTF-8",
 					"root", "123456");
 
-			Statement statement = connection.createStatement();
-			// CREATE A TABLE EMPOYEE
-			//int n = statement.executeUpdate("CREATE TABLE employee(emp_id int, emp_name varchar(30), emp_salary int)");
-			
-			// INSERT A NEW ROW INSERT COMMAND
-			//int n = statement.executeUpdate("INSERT INTO employee VALUES(103, 'Employee C', 7000)");
-			//int n = statement.executeUpdate("INSERT INTO employee(emp_id, emp_name) VALUES(104, 'Employee D')");
-			
-			// UPDATE ROW COMMAND
-			//int n = statement.executeUpdate("UPDATE employee SET emp_salary = 11000 WHERE emp_id = 104");
-			//int n = statement.executeUpdate("UPDATE employee SET emp_salary = 5000 WHERE emp_salary > 1000");
-			
-			// DELETE RECORD COMMAND
-			int n = statement.executeUpdate("DELETE FROM empoyee WHERE emp_id = 104");
+			// Insert using PreparedStatement 
+			PreparedStatement ps = connection.prepareStatement("INSERT INTO employee VALUES(?, ?, ?)");
+			ps.setInt(1, 103);
+			ps.setString(2, "EmployeeC");
+			ps.setInt(3, 10000);
+			int n = ps.executeUpdate();
 			
 			System.out.println("Number of rows affected: " + n);
 			
