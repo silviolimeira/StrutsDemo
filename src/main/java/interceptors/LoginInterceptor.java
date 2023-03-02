4db16e3a6457162cdf2e379811626d02d5817971
Silvio Limeira <sicemal@gmail.com>
2023-03-02 16:57:59 -0400

    Login Interceptor - Part 1

---
+++ src/main/java/interceptors/LoginInterceptor.java
@@ -0,0 +1,38 @@
+package interceptors;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.Interceptor;
+
+import action.LoginAction;
+
+public class LoginInterceptor implements Interceptor {
+
+	@Override
+	public void destroy() {
+		System.out.println("LoginInterceptor destroy() called");
+	}
+
+	@Override
+	public void init() {
+		System.out.println("LoginInterceptor init() called");
+	}
+
+	@Override
+	public String intercept(ActionInvocation ai) throws Exception {
+
+		Object user = ServletActionContext.getContext().getSession().get("loggedinUser");
+		
+		if (user == null) {
+			if (ai.getAction().getClass().equals(LoginAction.class)) {
+				return ai.invoke();
+			}
+			return "input";
+		}
+		
+		return ai.invoke();
+		
+	}
+
+}
